e891c69b24501c05f2d6db3fe0334dcb92366e97
Englert Ervin  Brunó <englert@users.noreply.github.com>
2024-01-28 14:00:13 +0000

    3

--- Home.py
+++ Home.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello99",
+    page_title="HelloHome",
     page_icon="👋",
 )
 
-st.title("Main Page")
+st.title("Home Main Page")
 st.sidebar.success("Select a page above.")
 
 
